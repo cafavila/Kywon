83f6ffa28ecd827835e939a23640559c02833e93
Cesar Avila Frias <cafavila@gmail.com>
2019-08-31 16:03:46 -0400

    Configuracion de componentes base para desarrollo

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'kywon.app.bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {loader: 'babel-loader'}
+        },
+        {
+            test: /\.html$/,
+            use: {loader: 'html-loader'}
+        },
+        {
+            test: /.\(s*)css$/,
+            use: [{loader: MiniCssExtractPlugin.loader}, 'css-loader', 'sass-loader']
+        }]
+    },
+    devServer: {
+        port: 9001,
+        historyApiFallback: true
+    },
+    plugins: [new HtmlWebPackPlugin({
+        template: './public/index.html',
+        filename: './index.html'
+    }),
+        new MiniCssExtractPlugin({filename: 'assets/[name].css'})]
+}
